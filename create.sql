638246254f94c5f6375930e581033dab4e109020
Sonya Pobiedimska <72732389+pobiedimskaya@users.noreply.github.com>
2021-12-28 15:25:52 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Sales
+(
+    sale_id      int    NOT NULL,
+    sale_price   int    NOT NULL
+);
+
+CREATE TABLE Houses
+(
+    house_id     int     NOT NULL,	
+    year_built   int     NOT NULL,
+	neighborhood_id int  NOT NULL,
+	sale_id      int     NOT NULL
+);
+
+CREATE TABLE Neighborhoods
+(
+    neighborhood_id      int        NOT NULL,	
+    neighborhood_name    char(20)	NOT NULL
+);
+
+----------------------
+-- Define primary keys
+----------------------
+ALTER TABLE Sales ADD CONSTRAINT PK_Sales PRIMARY KEY (sale_id);
+ALTER TABLE Houses ADD CONSTRAINT PK_Houses PRIMARY KEY (house_id);
+ALTER TABLE Neighborhoods ADD CONSTRAINT PK_Neighborhoods PRIMARY KEY (neighborhood_id);
+
+----------------------
+-- Define foreign keys
+----------------------
+ALTER TABLE Houses
+ADD CONSTRAINT FK_Houses_Sales FOREIGN KEY (sale_id) REFERENCES Sales (sale_id);
+ALTER TABLE Houses
+ADD CONSTRAINT FK_Houses_Neighborhoods FOREIGN KEY (neighborhood_id) REFERENCES Neighborhoods (neighborhood_id);
