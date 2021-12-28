7b64bcc1d7300673465bc8641ab2cc7f095611da
Sonya Pobiedimska <72732389+pobiedimskaya@users.noreply.github.com>
2021-12-28 19:29:51 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,26 +1,26 @@
 ----------------------------
 -- Populate Sales table
 ----------------------------
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(101, 100000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(102, 500000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(103, 350000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(104, 430000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(105, 210000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(106, 400000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(107, 300000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(108, 250000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(109, 630000);
-INSERT INTO Sales(sale_id, sale_price)
-VALUES(110, 320000);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(101, 100000, 201);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(102, 500000, 202);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(103, 350000, 203);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(104, 430000, 204);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(105, 210000, 205);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(106, 400000, 206);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(107, 300000, 207);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(108, 250000, 208);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(109, 630000, 209);
+INSERT INTO Sales(sale_id, sale_price, house_id)
+VALUES(110, 320000, 210);
 
 ----------------------------
 -- Populate Neighborhoods table
@@ -40,23 +40,23 @@ VALUES(305, 'Obolon');
 ----------------------------
 -- Populate Houses table
 ----------------------------
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(201, 2020, 301, 101);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(202, 2010, 301, 102);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(203, 2010, 302, 103);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(204, 2012, 303, 104);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(205, 2013, 303, 105);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(206, 2011, 303, 101);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(207, 2012, 304, 102);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(208, 2010, 304, 103);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(209, 2010, 305, 104);
-INSERT INTO Houses(house_id, year_built, neighborhood_id, sale_id)
-VALUES(210, 2015, 305, 105);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(201, 2020, 301);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(202, 2010, 301);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(203, 2010, 302);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(204, 2012, 303);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(205, 2013, 303);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(206, 2011, 303);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(207, 2012, 304);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(208, 2010, 304);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(209, 2010, 305);
+INSERT INTO Houses(house_id, year_built, neighborhood_id)
+VALUES(210, 2015, 305);
