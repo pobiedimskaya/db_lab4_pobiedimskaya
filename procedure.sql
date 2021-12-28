a0b00628a5a988eed5db961d1c6b453d1aed794b
Sonya Pobiedimska <72732389+pobiedimskaya@users.noreply.github.com>
2021-12-28 15:52:15 +0200

    Create procedure.sql

---
+++ procedure.sql
@@ -0,0 +1,15 @@
+-- Процедура, що визначає середню ціну за дім в залежності від року побуудови
+
+CREATE OR REPLACE PROCEDURE avetage_price_by_year(house_year int) 
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE average_price sales.sale_price%TYPE;
+
+BEGIN
+	SELECT AVG(sale_price) INTO average_price
+	FROM sales
+	JOIN houses USING(sale_id)
+	WHERE houses.year_built = house_year;
+    RAISE INFO 'Year built: %,  Average price: %', year_built, average_price;
+END;
+$$;
