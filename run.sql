392ed6fffc1ac25cde31fc5c8b04a0a059e89d8e
Sonya Pobiedimska <72732389+pobiedimskaya@users.noreply.github.com>
2021-12-30 16:26:33 +0200

    Update procedure

--- run.sql
+++ run.sql
@@ -19,7 +19,7 @@ select average_sale_price(10)
 -- procedure
 -- Процедура, що визначає середню ціну за дім в залежності від року побуудови
 
-CREATE OR REPLACE PROCEDURE average_price_by_year(house_year int) 
+CREATE OR REPLACE PROCEDURE avetage_price_by_year(house_year int) 
 LANGUAGE 'plpgsql'
 AS $$
 DECLARE average_price sales.sale_price%TYPE;
@@ -27,9 +27,9 @@ DECLARE average_price sales.sale_price%TYPE;
 BEGIN
 	SELECT AVG(sale_price) INTO average_price
 	FROM sales
-	JOIN houses USING(sale_id)
+	JOIN houses USING(house_id)
 	WHERE houses.year_built = house_year;
-    RAISE INFO 'Year built: %,  Average price: %', year_built, average_price;
+    RAISE INFO 'Year built: %,  Average price: %', house_year, average_price;
 END;
 $$;
 
